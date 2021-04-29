e6380bd19d4ccc37e526181aaedb081bacc33ad7
seki <okiu_soccer7@icloud.com>
2021-04-29 16:00:51 +0900

    不要なif文削除

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -201,45 +201,42 @@ class MypageController extends Controller
         $flg2 = 1;
         $flg3 = 1;
 
-        foreach($tokens as $token) {
-            // if($token->delete_flg == '0') {
-                if(date("H:i") == '06:30' && $flg1 == 1) {
-                    foreach($tokens as $token) {
-                        if($token->delete_flg == '0') {
-                            AutoFallowUtil::autotweet($token->access_token, $token->access_token_secret, $comment1);
-                            sleep(180);
-                        }
-                    }
-                    $flg1 = 0;
+        if(date("H:i") == '06:30' && $flg1 == 1) {
+            foreach($tokens as $token) {
+                if($token->delete_flg == '0') {
+                    AutoFallowUtil::autotweet($token->access_token, $token->access_token_secret, $comment1);
                     sleep(180);
-                    $flg1 = 1;
+                }
+            }
+            $flg1 = 0;
+            sleep(180);
+            $flg1 = 1;
 
-                }
-                if(date("H:i") == '12:30' && $flg2 == 1) {
-                    foreach($tokens as $token) {
-                        if($token->delete_flg == '0') {
-                            AutoFallowUtil::autotweet($token->access_token, $token->access_token_secret, $comment2);
-                            sleep(180);
-                        }
-                    }
-                    $flg1 = 0;
+        }
+        if(date("H:i") == '12:30' && $flg2 == 1) {
+            foreach($tokens as $token) {
+                if($token->delete_flg == '0') {
+                    AutoFallowUtil::autotweet($token->access_token, $token->access_token_secret, $comment2);
                     sleep(180);
-                    $flg2 = 1;
+                }
+            }
+            $flg1 = 0;
+            sleep(180);
+            $flg2 = 1;
 
-                }
-                if(date("H:i") == '19:30' && $flg3 == 1) {
-                    foreach($tokens as $token) {
-                        if($token->delete_flg == '0') {
-                            AutoFallowUtil::autotweet($token->access_token, $token->access_token_secret, $comment3);
-                            sleep(180);
-                        }
-                    }
-                    $flg1 = 0;
+        }
+        if(date("H:i") == '19:30' && $flg3 == 1) {
+            foreach($tokens as $token) {
+                if($token->delete_flg == '0') {
+                    AutoFallowUtil::autotweet($token->access_token, $token->access_token_secret, $comment3);
                     sleep(180);
-                    $flg1 = 1;
                 }
-            // }
-        }  
+            }
+            $flg1 = 0;
+            sleep(180);
+            $flg1 = 1;
+        }
+
         return view('mypage.debug', ['tokens' => $tokens, 'date' => date("H:i")]);
     }
 }
