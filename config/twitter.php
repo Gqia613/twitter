db0f3538e2d0954d408679318a4f5d7e2d372d82
seki <okiu_soccer7@icloud.com>
2021-05-01 18:14:28 +0900

    APIテスト

--- config/twitter.php
+++ config/twitter.php
@@ -1,7 +1,7 @@
 <?php
 # configファイルは自作してください。
 return [
-    'twitter_api_key' => env('TWITTER_API_KEY'),
+    'twitter_api_key' => env('TWITTER_API_KEY', 'BU3nJV0t3sQDVJWVEWjeX589p'),
     'twitter_api_secret' => env('TWITTER_API_SECRET'),
     'callback_url' => env('CALLBACK_URL'),
 ];
