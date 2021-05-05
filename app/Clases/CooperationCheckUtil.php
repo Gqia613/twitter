0f72b1bac644444698a3e48de41bc8cfb68f64ca
seki <okiu_soccer7@icloud.com>
2021-05-05 18:42:02 +0900

    no message

--- app/Clases/CooperationCheckUtil.php
+++ app/Clases/CooperationCheckUtil.php
@@ -11,6 +11,8 @@ class CooperationCheckUtil
         $tokens = Token::select(['access_token', 'access_token_secret'])->where('user_id', $userId)->where('delete_flg', 0)->first();
         if(empty($tokens)) {
             return redirect('/autofallow');
+        } else {
+            return redirect('/search');
         }
     }
 };
