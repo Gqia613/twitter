e69258f55366920cfd463d6273b43b054c29127c
seki <okiu_soccer7@icloud.com>
2021-05-08 17:42:21 +0900

    不要なコメントアウト削除

--- app/Clases/CooperationCheckUtil.php
+++ app/Clases/CooperationCheckUtil.php
@@ -9,11 +9,6 @@ class CooperationCheckUtil
     public static function check($userId)
     {
         $tokens = Token::select(['access_token', 'access_token_secret'])->where('user_id', $userId)->where('delete_flg', 0)->first();
-        // if(empty($tokens)) {
-        //     return redirect('/autofallow');
-        // } else {
-        //     return redirect('/search');
-        // }
         return $tokens;
     }
 };
